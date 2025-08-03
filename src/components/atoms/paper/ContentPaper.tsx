a6887e79d5e2c61a45edd0bcf86efa760970d849
xyzuan <xyzuannihboss@gmail.com>
2025-08-03 20:56:27 +0700

    feat(): Update Content Paper ui

--- src/components/atoms/paper/ContentPaper.tsx
+++ src/components/atoms/paper/ContentPaper.tsx
@@ -1,18 +1,31 @@
-import { PageContainer, ProCard } from "@ant-design/pro-components";
-import { Skeleton } from "antd";
+import { Breadcrumb, Card, Skeleton, Spin } from "antd";
 import { ReactNode } from "react";
 import { Helmet } from "react-helmet-async";
+import { Typography } from "../typography";
+import { Link } from "react-router";
 
 type ContentPaperProps = {
   children: ReactNode;
   hideTitle?: boolean;
   isLoading?: boolean;
+  breadcrumb?: {
+    name: string;
+    link?: string;
+  }[];
   title?: string;
   action?: ReactNode;
   translucent?: boolean;
 };
 
-const ContentPaper = ({ children, isLoading, title }: ContentPaperProps) => {
+const ContentPaper = ({
+  children,
+  isLoading,
+  title,
+  hideTitle,
+  action,
+  breadcrumb,
+  translucent = true,
+}: ContentPaperProps) => {
   return (
     <>
       {title && (
@@ -20,11 +33,33 @@ const ContentPaper = ({ children, isLoading, title }: ContentPaperProps) => {
           <title>{isLoading ? "Loading..." : title}</title>
         </Helmet>
       )}
-      <PageContainer>
-        <ProCard>
+      <Spin spinning={isLoading || false}>
+        <Card
+          variant="borderless"
+          className={`${
+            translucent ? "bg-white/40 backdrop-blur-2xl" : "bg-white/85"
+          }`}
+        >
+          <Breadcrumb className="mb-3">
+            {breadcrumb?.map((item, index) => (
+              <Breadcrumb.Item key={index}>
+                {item.link ? (
+                  <Link to={item.link}>{item.name}</Link>
+                ) : (
+                  item.name
+                )}
+              </Breadcrumb.Item>
+            ))}
+          </Breadcrumb>
+          {title && !hideTitle && (
+            <div className="flex md:flex-row flex-col justify-between  mb-3">
+              <Typography.H3>{isLoading ? "Loading..." : title}</Typography.H3>
+              {action}
+            </div>
+          )}
           {isLoading ? <Skeleton active paragraph={{ rows: 12 }} /> : children}
-        </ProCard>
-      </PageContainer>
+        </Card>
+      </Spin>
     </>
   );
 };
