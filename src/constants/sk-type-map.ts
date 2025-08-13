c587c56fc122c5b1364f97381a127fd3e1fda55e
xyzuan <xyzuannihboss@gmail.com>
2025-08-14 00:07:25 +0700

    feat(): Update SK Type map

--- src/constants/sk-type-map.ts
+++ src/constants/sk-type-map.ts
@@ -3,4 +3,11 @@ import { SkType } from "@/interfaces/services/sk-type";
 export const SK_TYPE_MAP: Record<SkType, string> = {
   TIDAK_MAMPU: "Surat Keterangan Tidak Mampu",
   KEMATIAN: "Surat Keterangan Kematian",
+  BEDA_NAMA: "Surat Keterangan Beda Nama",
+  KELAHIRAN: "Surat Keterangan Kelahiran",
+  DOMISILI: "Surat Keterangan Domisili",
+  KEHILANGAN: "Surat Keterangan Kehilangan",
+  DISPENSASI: "Surat Keterangan Dispensasi",
+  KTP_SEMENTARA: "Surat Keterangan KTP Sementara",
+  USAHA: "Surat Keterangan Usaha",
 } as const;
